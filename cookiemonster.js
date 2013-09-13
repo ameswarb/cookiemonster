6ef05e329c73794ca9e7b5dd7774b738766e4169
Alex Meswarb <alex.meswarb@firehost.com>
2013-09-13 17:19:11 -0500

    initial commit
    initial commit

---
+++ cookiemonster.js
@@ -0,0 +1,328 @@
+var upgradeLookup = {"data": [
+        {"id": 0, "type": "object", "object": "Cursor"},
+        {"id": 1, "type": "object", "object": "Cursor"},
+        {"id": 2, "type": "object", "object": "Cursor"},
+        {"id": 3, "type": "object", "object": "Cursor"},
+        {"id": 4, "type": "object", "object": "Cursor"},
+        {"id": 5, "type": "object", "object": "Cursor"},
+        {"id": 6, "type": "object", "object": "Cursor"},
+        {"id": 7, "type": "object", "object": "Grandma"},
+        {"id": 8, "type": "object", "object": "Grandma"},
+        {"id": 9, "type": "object", "object": "Grandma"},
+        {"id": 10, "type": "object", "object": "Farm"},
+        {"id": 11, "type": "object", "object": "Farm"},
+        {"id": 12, "type": "object", "object": "Farm"},
+        {"id": 13, "type": "object", "object": "Factory"},
+        {"id": 14, "type": "object", "object": "Factory"},
+        {"id": 15, "type": "object", "object": "Factory"},
+        {"id": 16, "type": "object", "object": "Mine"},
+        {"id": 17, "type": "object", "object": "Mine"},
+        {"id": 18, "type": "object", "object": "Mine"},
+        {"id": 19, "type": "object", "object": "Shipment"},
+        {"id": 20, "type": "object", "object": "Shipment"},
+        {"id": 21, "type": "object", "object": "Shipment"},
+        {"id": 22, "type": "object", "object": "Alchemy lab"},
+        {"id": 23, "type": "object", "object": "Alchemy lab"},
+        {"id": 24, "type": "object", "object": "Alchemy lab"},
+        {"id": 25, "type": "object", "object": "Portal"},
+        {"id": 26, "type": "object", "object": "Portal"},
+        {"id": 27, "type": "object", "object": "Portal"},
+        {"id": 28, "type": "object", "object": "Time machine"},
+        {"id": 29, "type": "object", "object": "Time machine"},
+        {"id": 30, "type": "object", "object": "Time machine"},
+        {"id": 31, "type": "global_multiplier", "objectName": ''},
+        {"id": 32, "type": "global_multiplier", "objectName": ''},
+        {"id": 33, "type": "global_multiplier", "objectName": ''},
+        {"id": 34, "type": "global_multiplier", "objectName": ''},
+        {"id": 35, "type": "global_multiplier", "objectName": ''},
+        {"id": 36, "type": "global_multiplier", "objectName": ''},
+        {"id": 37, "type": "global_multiplier", "objectName": ''},
+        {"id": 38, "type": "global_multiplier", "objectName": ''},
+        {"id": 39, "type": "global_multiplier", "objectName": ''},
+        {"id": 40, "type": "global_multiplier", "objectName": ''},
+        {"id": 41, "type": "global_multiplier", "objectName": ''},
+        {"id": 42, "type": "global_multiplier", "objectName": ''},
+        {"id": 43, "type": "object", "object": "Cursor"},
+        {"id": 44, "type": "object", "object": "Grandma"},
+        {"id": 45, "type": "object", "object": "Farm"},
+        {"id": 46, "type": "object", "object": "Factory"},
+        {"id": 47, "type": "object", "object": "Mine"},
+        {"id": 48, "type": "object", "object": "Shipment"},
+        {"id": 49, "type": "object", "object": "Alchemy lab"},
+        {"id": 50, "type": "object", "object": "Portal"},
+        {"id": 51, "type": "object", "object": "Time machine"},
+        {"id": 52, "type": "object", "gold_cookie_multiplier": ''},
+        {"id": 53, "type": "object", "gold_cookie_multiplier": ''},
+        {"id": 54, "type": "global_multiplier", "object": ''},
+        {"id": 55, "type": "global_multiplier", "object": ''},
+        {"id": 56, "type": "global_multiplier", "object": ''},
+        {"id": 57, "type": "object", "object": "Grandma"},
+        {"id": 58, "type": "object", "object": "Grandma"},
+        {"id": 59, "type": "object", "object": "Grandma"},
+        {"id": 60, "type": "object", "object": "Grandma"},
+        {"id": 61, "type": "object", "object": "Grandma"},
+        {"id": 62, "type": "object", "object": "Grandma"},
+        {"id": 63, "type": "object", "object": "Grandma"},
+        {"id": 64, "type": "object", "object": "Grandma"},
+        {"id": 65, "type": "global_multiplier", "object": ''},
+        {"id": 66, "type": "global_multiplier", "object": ''},
+        {"id": 67, "type": "object", "object": "Grandma"},
+        {"id": 68, "type": "global_multiplier", "object": ''},
+        {"id": 69, "type": "object", "object": "Grandma"},
+        {"id": 70, "type": "global_multiplier", "object": ''},
+        {"id": 71, "type": "object", "object": "Grandma"},
+        {"id": 72, "type": "global_multiplier", "object": ''},
+        {"id": 73, "type": "object", "object": "Grandma"},
+        {"id": 74, "type": "object", "object": "Grandma"},
+        {"id": 75, "type": "cursor_multiplier", "object": ''},
+        {"id": 76, "type": "cursor_multiplier", "object": ''},
+        {"id": 77, "type": "cursor_multiplier", "object": ''},
+        {"id": 78, "type": "cursor_multiplier", "object": ''},
+        {"id": 79, "type": '', "object": ''},
+        {"id": 80, "type": "global_multiplier", "object": ''},
+        {"id": 81, "type": "global_multiplier", "object": ''},
+        {"id": 82, "type": "object", "object": "Cursor"},
+        {"id": 83, "type": "gold_cookie_multiplier", "object": ''},
+        {"id": 84, "type": "global_multiplier", "object": ''},
+        {"id": 85, "type": "global_multiplier", "object": ''},
+        {"id": 86, "type": "gold_cookie_multiplier", "object": ''},
+        {"id": 87, "type": "global_multiplier", "object": ''},
+        {"id": 88, "type": "global_multiplier", "object": ''},
+        {"id": 89, "type": "global_multiplier", "object": ''},
+        {"id": 90, "type": "global_multiplier", "object": ''},
+        {"id": 91, "type": '', "object": ''},
+        {"id": 92, "type": "global_multiplier", "object": ''},
+        {"id": 93, "type": "global_multiplier", "object": ''},
+        {"id": 94, "type": "global_multiplier", "object": ''},
+        {"id": 95, "type": "global_multiplier", "object": ''},
+        {"id": 96, "type": "global_multiplier", "object": ''},
+        {"id": 97, "type": "global_multiplier", "object": ''},
+        {"id": 98, "type": "global_multiplier", "object": ''}
+    ]};
+ 
+ 
+function catchGoldenCookies() {
+    if (Game.goldenCookie.life > 0) {
+        // click golden cookies if they're available
+        Game.goldenCookie.click();
+        console.log("caught a golden cookie!!!");
+    }
+}
+ 
+ 
+function fetchGameObjects() {
+    eligibleGameObjects = new Array();
+    for (var i in Game.ObjectsById)
+    {
+        var me=Game.ObjectsById[i];
+        if (me.price < Game.cookies) {
+            eligibleGameObjects.push(me);
+        }
+    }
+    return eligibleGameObjects;
+}
+ 
+ 
+function fetchGameUpgrades() {
+    eligibleGameUpgrades = new Array();
+    for (var i in Game.UpgradesById)
+    {
+        var me=Game.UpgradesById[i];
+        if (me.unlocked == 1 && me.bought != 1 && me.basePrice < Game.cookies) {
+            eligibleGameUpgrades.push(me);
+        }
+    }
+    return eligibleGameUpgrades;
+}
+ 
+ 
+function getGameObjectIDByName(name) {
+    for (var i in Game.ObjectsById)
+    {
+        var me=Game.ObjectsById[i];
+        if (me.name == name) {
+            return i;
+        }
+    }
+    return false;
+}
+ 
+ 
+function calculateMultiplier() {
+    mult = 1;
+    for (var i in Game.Upgrades)
+    {
+        var me=Game.Upgrades[i];
+        if (me.bought>0)
+        {
+            if (me.type=='cookie' && Game.Has(me.name)) mult+=me.power*0.01;
+        }
+    }
+ 
+    /*
+    mult+=(Game.Has('Oatmeal raisin cookies')+Game.Has('Peanut butter cookies')+Game.Has('Plain cookies')+Game.Has('Coconut cookies'))*0.05;
+    mult+=(Game.Has('White chocolate cookies')+Game.Has('Macadamia nut cookies')+Game.Has('Sugar cookies'))*0.05;
+    mult+=(Game.Has('Double-chip cookies')+Game.Has('White chocolate macadamia nut cookies')+Game.Has('All-chocolate cookies'))*0.1;
+    mult+=(Game.Has('Dark chocolate-coated cookies')+Game.Has('White chocolate-coated cookies'))*0.15;
+    mult+=(Game.Has('Zebra cookies')+Game.Has('Eclipse cookies'))*0.15;
+    mult+=(Game.Has('Snickerdoodles')+Game.Has('Stroopwafels')+Game.Has('Macaroons'))*0.15;
+    */
+    mult+=Game.Has('Specialized chocolate chips')*0.01;
+    mult+=Game.Has('Designer cocoa beans')*0.02;
+    mult+=Game.Has('Underworld ovens')*0.03;
+    mult+=Game.Has('Exotic nuts')*0.04;
+    mult+=Game.Has('Arcane sugar')*0.05;
+   
+    if (!Game.prestige.ready) Game.CalculatePrestige();
+    mult+=parseInt(Game.prestige['Heavenly chips'])*0.05;
+ 
+    if (Game.Has('Kitten helpers')) mult*=(1+Game.milkProgress*0.05);
+    if (Game.Has('Kitten workers')) mult*=(1+Game.milkProgress*0.1);
+    if (Game.Has('Kitten engineers')) mult*=(1+Game.milkProgress*0.2);
+   
+    if (Game.frenzy>0) mult*=Game.frenzyPower;
+   
+    if (Game.Has('Elder Covenant')) mult*=0.95;
+ 
+    return mult;
+}
+ 
+function predictGains(type, id, gameObject) {
+ 
+    if(type == "object") {
+        game_id = getGameObjectIDByName(gameObject);
+        //currentObject = Game.ObjectsByID[game_id];
+        currentObject = Game.ObjectsById[game_id];
+        if (currentObject.amount > 0) {
+            estimatedGains = currentObject.storedTotalCps / currentObject.amount;
+        } else if (typeof currentObject.cps != 'undefined') {
+            estimatedGains = currentObject.cps();
+        } else {
+            estimatedGains = 99999; //exception, buy it
+            console.log("exception, object doesn't have cps function");
+        }
+       
+    }
+ 
+    if(type == "upgrade") {
+        currentLookup = upgradeLookup.data[id];
+ 
+        if (currentLookup.type == "gold_cookie_multiplier") {
+            // prefer gold cookie multipliers above all else as we'll be catching all of them
+            estimatedGains = 99999999999;
+        }
+ 
+        if (currentLookup.type == "cursor_multiplier") {
+            // too lazy to calculate for these
+            estimatedGains = 99999999;
+        }
+ 
+        if (currentLookup.type == "object") {
+            currentObjectID = getGameObjectIDByName(currentLookup.object);
+            //console.log(currentLookup.object);
+            currentObject = Game.ObjectsById[currentObjectID];
+            currentUpgrade= Game.UpgradesById[id];
+            if (typeof currentObject.cps != 'undefined') {
+                currentCpsPerUnit = currentObject.cps();
+            } else {
+                currentCpsPerUnit = 9999999;
+                console.log("exception, upgrade doesn't have cps function");
+            }            
+ 
+            currentUpgrade.bought = 1;
+            if (typeof currentObject.cps != 'undefined') {
+                newCpsPerUnit = currentObject.cps();
+            } else {
+                newCpsPerUnit = 999999;
+                console.log("exception, upgrade doesn't have cps function");
+            }            
+            currentUpgrade.bought = 0;
+ 
+            currentTotalCps = currentCpsPerUnit * currentObject.amount;
+            newTotalCps = newCpsPerUnit * currentObject.amount;
+ 
+            estimatedGains = newTotalCps - currentTotalCps;
+        }
+ 
+        if (currentLookup.type == "global_multiplier") {
+            currentUpgrade = Game.UpgradesById[id];
+            currentMultiplier = calculateMultiplier();
+   
+            currentUpgrade.bought = 1;
+            newMultiplier = calculateMultiplier();
+            currentUpgrade.bought = 0;
+ 
+            baseCps = Game.cookiesPs / currentMultiplier;
+            newCps = baseCps * newMultiplier;
+ 
+            estimatedGains = newCps - baseCps;
+        }
+       
+    }
+ 
+    //console.log(estimatedGains);
+ 
+    return estimatedGains;
+}
+ 
+ 
+function analyzeCostBenefit() {
+    costBenefits = new Array();
+ 
+    eligibleGameObjects = fetchGameObjects();
+    for (var i in eligibleGameObjects)
+    {
+        currentGameObject = eligibleGameObjects[i];
+        predictedGains = predictGains("object", '', currentGameObject.name);
+        currentCostBenefit = {"type": "object", "id": currentGameObject.id, "gains": predictedGains}
+        costBenefits.push(currentCostBenefit);
+        //console.log(currentGameObject.id);
+    }
+ 
+    eligibleGameUpgrades = fetchGameUpgrades();
+    for (var i in eligibleGameUpgrades)
+    {
+        currentGameUpgrade = eligibleGameUpgrades[i];
+        predictedGains = predictGains("upgrade", currentGameUpgrade.id);
+        currentCostBenefit = {"type": "upgrade", "id": currentGameUpgrade.id, "gains": predictedGains}
+        costBenefits.push(currentCostBenefit);
+        //console.log(currentGameUpgrade.id);
+    }
+ 
+    costBenefits.sort(function(a,b) {
+        // sorts costBenefits array by gains descending
+        return b.gains - a.gains;
+    });
+ 
+    return costBenefits;
+}
+ 
+ 
+function buySomething() {
+    costBenefits = analyzeCostBenefit();
+    selection = costBenefits[0];
+    if (selection.type == "object") {
+        selectedProduct = Game.ObjectsById[selection.id];
+        console.log("bought " + selectedProduct.name);
+        selectedProduct.buy();
+    } else if (selection.type == "upgrade") {
+        selectedProduct = Game.UpgradesById[selection.id];
+        console.log("bought " + selectedProduct.name);
+        selectedProduct.buy();
+    }
+}
+ 
+var clickIter = 0;
+var buyCounter = 0;
+ 
+ 
+function runBot() {
+    setInterval(function(){
+        catchGoldenCookies();
+        Game.ClickCookie();
+        buyCounter = buyCounter + 1;
+        if (buyCounter % 100 == 0) {
+            buySomething();
+        }
+    },100)
+}
